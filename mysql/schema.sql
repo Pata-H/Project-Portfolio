d706f058b857d528bd70c26e22834fea476f4a07
Pata-H <Pata.Hikari@gmail.com>
2018-07-15 00:38:26 -0700

    Upload

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE info(
-    name VARCHAR(20) NULL
-    birthday DATE NULL
-    height VARCHAR(20)
-    sex CHAR(4)
-    food VARCHAR(20)
+    'name' VARCHAR(20) NULL
+    'birthday' DATE NULL
+    'height' VARCHAR(45) NULL
+    'sex' CHAR(4) NULL
+    'food' VARCHAR(45) NULL 
 );
