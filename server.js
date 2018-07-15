28aed4fb2d625308cea810cd3bddc69f38dc1d2e
Pata-H <Pata.Hikari@gmail.com>
2018-07-15 01:23:25 -0700

    Upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
